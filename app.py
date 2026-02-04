c3b698c0f08541eb79a240a56ba3d04f7ce5b6ef
htinkyawzaw2017-maker <htinkyawzaw2017@gmail.com>
2026-02-04 23:25:41 +0630

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ st.markdown("""
 st.markdown("""
 <div style="display: flex; justify-content: center; align-items: center; gap: 20px;">
     <img src="https://img.icons8.com/nolan/96/movie-projector.png" width="80"/>
-    <div class="main-header">Myanmar AI Studio Pro</div>
+    <div class="main-header"></div>
 </div>
 """, unsafe_allow_html=True)
 
